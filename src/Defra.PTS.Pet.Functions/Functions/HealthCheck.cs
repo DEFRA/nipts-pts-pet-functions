6fe49724e836b29a0afb7f6f844fa2d69382e27b
deanfmbrowncapgemini <dean.fm.brown@capgemini.com>
2026-04-21 15:07:08 +0100

    * 'Line 20: Removed the usage of OpenApiResponseWithBodyAttribute because the type does not exist in the project or referenced assemblies. No direct replacement is available based on the current project context.' in file 'src\Defra.PTS.Pet.Functions\Functions\HealthCheck.cs'

--- src/Defra.PTS.Pet.Functions/Functions/HealthCheck.cs
+++ src/Defra.PTS.Pet.Functions/Functions/HealthCheck.cs
@@ -17,8 +17,7 @@ public class HealthCheck(IPetService petService)
 
     [Function("HealthCheck")]
     // [OpenApiOperation(operationId: "Run", tags: "name")]
-    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
 #pragma warning disable IDE0060 // Remove unused parameter
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health")] HttpRequest req, ILogger log)
 #pragma warning restore IDE0060 // Remove unused parameter

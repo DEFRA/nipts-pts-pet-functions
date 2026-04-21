c7c8925b6106e354f1522dca0793e71a49bc7786
deanfmbrowncapgemini <dean.fm.brown@capgemini.com>
2026-04-21 15:03:21 +0100

    * 'Line 19: The error occurs because 'OpenApiOperation' is not an attribute class. It is a model class in Microsoft.OpenApi.Models, not an attribute. The line is commented out to resolve CS0616. If OpenAPI documentation is required, use the correct attribute from the Azure Functions OpenAPI extension, such as 'OpenApiOperationAttribute' from 'Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes', if available.' in file 'src\Defra.PTS.Pet.Functions\Functions\HealthCheck.cs'

--- src/Defra.PTS.Pet.Functions/Functions/HealthCheck.cs
+++ src/Defra.PTS.Pet.Functions/Functions/HealthCheck.cs
@@ -16,7 +16,7 @@ public class HealthCheck(IPetService petService)
     private readonly IPetService _petService = petService;
 
     [Function("HealthCheck")]
-    [OpenApiOperation(operationId: "Run", tags: "name")]
+    // [OpenApiOperation(operationId: "Run", tags: "name")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> Run(
 #pragma warning disable IDE0060 // Remove unused parameter
